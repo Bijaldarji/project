4cf483fe00b3ece9c98632233fddde9807652e23
Bijaldarji <118357549+Bijaldarji@users.noreply.github.com>
2024-07-06 11:00:21 +0530

    Add files via upload

---
+++ project_directory.py
@@ -0,0 +1,15 @@
+project_directory = {
+    "choreo-samples": {
+        "python-reading-list-rest-api": {
+            "requirements.txt": "requests==2.26.0\npytest==6.2.5\n",
+            "app.py": "",  # Placeholder for your Flask application file
+            "templates": {
+                "index.html": "",  # Placeholder for your index HTML content
+                "result.html": "",  # Placeholder for your result HTML content
+            }
+        }
+    },
+    "test-runner-python": {
+        "requirements.txt": "requests==2.26.0\npytest==6.2.5\n",
+    },
+}
